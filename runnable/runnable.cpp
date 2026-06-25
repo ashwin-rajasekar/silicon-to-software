d72ae4f40d70d2479d05b99137a864bd1040a88d
Ashwin Rajasekar <ashraj101010@gmail.com>
2026-06-25 12:37:56 +0000

    add runnable example

---
+++ runnable/runnable.cpp
@@ -0,0 +1,33 @@
+// char const    src_string[] = "hello world ";
+// char volatile dst_string[] = "something else";
+
+int main(){
+	// char const* src = src_string;
+	// char volatile* dst = dst_string;
+	// while(*src){ *dst++ = *src++; }
+}
+
+using handler_ptr = void(*const)() noexcept;
+
+void trap() noexcept {
+	while(true){ asm volatile ("wfi"); }
+}
+
+extern char __data_dst[], __bss_dst[];
+extern char const __data_src[], __data_end[], __bss_end[];
+
+[[gnu::naked]] void reset() noexcept {
+	char const* src = __data_src;
+	for(char* dst = __data_dst; dst < __data_end; *dst++ = *src++); 
+	for(char* dst = __bss_dst;  dst < __bss_end;  *dst++ = 0);
+
+	extern int main() noexcept;
+	main();
+
+	trap();
+}
+
+[[gnu::section(".vectors"), gnu::used]] handler_ptr vectors[] = {
+	reset, trap, trap, trap, trap, trap, nullptr, nullptr,
+	nullptr, nullptr, trap, nullptr, nullptr, trap
+};
